425fc23aa512d52dc3ddcf52d85218a314d8f0a3
bkapil002 <kapilabrahm@gmail.com>
2025-09-02 18:22:23 +0545

    certificte

--- frame2/src/pages/AttendanceRequests.jsx
+++ frame2/src/pages/AttendanceRequests.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import dayjs from "dayjs";
 import z from "../components/LOGO.png";
+import PDFlogo from "../components/pdfLOGo.png";
 import jsPDF from "jspdf";
 import html2canvas from "html2canvas";
 
@@ -30,11 +31,11 @@ const AttendanceRequests = () => {
     let yPosition = margin;
 
     // === LOGO ===
-    const logoWidth = 60;
+    const logoWidth = 70;
     const logoHeight = 16;
     const logoX = (pageWidth - logoWidth) / 2;
     try {
-      pdf.addImage(z, "PNG", logoX, yPosition, logoWidth, logoHeight);
+      pdf.addImage(PDFlogo, "PNG", logoX, yPosition, logoWidth, logoHeight);
     } catch (err) {
       console.warn("Logo not found, skipping image");
     }
