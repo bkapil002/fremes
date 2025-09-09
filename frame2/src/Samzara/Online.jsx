c0bdc6c7b4755d9ecfaa0e871e6cfc5f71f7f3e0
bkapil002 <kapilabrahm@gmail.com>
2025-09-09 19:47:07 +0545

    online-shere

--- frame2/src/Samzara/Online.jsx
+++ frame2/src/Samzara/Online.jsx
@@ -14,11 +14,35 @@ const Online = ({ meetingTime, meetingtopic }) => {
   return (
     <div className="-mt-2">
       <div className=" p-4 space-y-2">
-        <div className="bg-white rounded-md shadow-sm p-2 md:p-3 flex items-center justify-center space-x-2">
-          <FaCircle className="text-green-600 text-[9px] md:text-xs" />
-          <span className="md:text-[25px] text-sm font-semibold text-[#2A2A72]">
-            Live Online Meetings
-          </span>
+        <div className="bg-white rounded-md shadow-sm p-2 md:p-3 flex items-center justify-between">
+          {/* Left & Center Content */}
+          <div className="flex flex-1 pl-15 justify-center items-center gap-2">
+            <FaCircle className="text-green-600 text-[9px] md:text-xs" />
+            <span className="md:text-[25px] text-sm font-semibold text-[#2A2A72]">
+              Live Online Meetings
+            </span>
+          </div>
+
+          {/* Right Content */}
+          <div className="flex items-center gap-1 cursor-pointer pr-2 text-indigo-900 font-semibold  text-[12px] md:text-sm "
+          aria-label="Share"
+          onClick={() => {
+              const shareData = {
+                url: window.location.href,
+              };
+
+              if (navigator.share) {
+                navigator.share(shareData).catch((error) => {});
+              } else {
+                const message = encodeURIComponent(
+                  `${shareData.text}\n${shareData.url}`
+                );
+                window.open(`https://wa.me/?text=${message}`, "_blank");
+              }
+            }}>
+            <FaShareAlt size={10} />
+            <span>Share</span>
+          </div>
         </div>
 
         <div className="bg-white rounded-md shadow-sm md:p-6  p-1">
@@ -28,7 +52,9 @@ const Online = ({ meetingTime, meetingtopic }) => {
               <div className="md:text-base text-sm font-semibold text-[#2A2A72]">
                 {meetingtopic}
               </div>
-              <div className="text-red-600 md:text-sm text-xs ">From :<span className=" font-semibold">({meetingTime})</span></div>
+              <div className="text-red-600 md:text-sm text-xs ">
+                From :<span className=" font-semibold">({meetingTime})</span>
+              </div>
             </div>
 
             {/* Middle Links */}
@@ -48,13 +74,17 @@ const Online = ({ meetingTime, meetingtopic }) => {
               </div>
               <div className="mt-1 text-sm">
                 <span className="md:text-sm text-xs">Meeting Topic:</span>{" "}
-                <span className="font-semibold md:text-sm text-xs">{meetingtopic}</span>
+                <span className="font-semibold md:text-sm text-xs">
+                  {meetingtopic}
+                </span>
               </div>
             </div>
 
             {/* Right Side */}
             <div className="text-center -mt-2 md:mt-0 md:text-right md:text-sm text-xs">
-              <span className="font-semibold md:text-sm text-xsmd:text-sm text-xs">Current Time:</span>{" "}
+              <span className="font-semibold md:text-sm text-xsmd:text-sm text-xs">
+                Current Time:
+              </span>{" "}
               <span className="text-red-600 font-semibold">
                 {getCurrentTimeCustom()}
               </span>
@@ -67,27 +97,6 @@ const Online = ({ meetingTime, meetingtopic }) => {
                 </button>
               </div>
             </div>
-          </div>
-          <div
-            aria-label="Share"
-            className="md:mt-3 mt-0 flex items-center space-x-2 text-indigo-900 font-semibold text-sm cursor-pointer md:-mb-3 -mb-0 "
-            onClick={() => {
-              const shareData = {
-                url: window.location.href,
-              };
-
-              if (navigator.share) {
-                navigator.share(shareData).catch((error) => {});
-              } else {
-                const message = encodeURIComponent(
-                  `${shareData.text}\n${shareData.url}`
-                );
-                window.open(`https://wa.me/?text=${message}`, "_blank");
-              }
-            }}
-          >
-            <FaShareAlt size={12} />
-            <span>Share</span>
           </div>
         </div>
       </div>
