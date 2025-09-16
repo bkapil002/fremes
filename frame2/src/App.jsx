7b387ef462c2a212184d0bc3cdc8fcca62acb427
bkapil002 <kapilabrahm@gmail.com>
2025-09-16 16:18:07 +0545

    error

--- frame2/src/App.jsx
+++ frame2/src/App.jsx
@@ -14,6 +14,7 @@ import Footer from "./components/Footer";
 import { Toaster } from 'react-hot-toast';
 import ProtectedRoute from "./context/ProtectedRoute";
 import Dashboard from "./components/Dashboard";
+import AttendanceNavigate from "./components/AttendanceNavigate";
 
 function App() {
 
