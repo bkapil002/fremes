8756b3c3a3cc1a889581f6806848de9953f4056a
bkapil002 <kapilabrahm@gmail.com>
2025-09-16 18:14:43 +0545

    changes

--- frame2/src/components/AttendanceNavigate.jsx
+++ frame2/src/components/AttendanceNavigate.jsx
@@ -4,13 +4,13 @@ import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 
 export default function AttendanceNavigate() {
-  const { email } = useParams(); // 👈 get `name` not `email`
+  const { email } = useParams(); 
   const [loading, setLoading] = useState(true);
  const [error, setError] = useState(null);
  const navigate = useNavigate();
-  const { user, login } = useAuth(); // get user from context
+  const {  login } = useAuth(); 
 
-  useEffect(() => {
+   useEffect(() => {
     if (!email) {
       setError("No name provided in URL");
       setLoading(false);
@@ -36,26 +36,37 @@ export default function AttendanceNavigate() {
 
         login({ ...data.user, token: data.token });
         navigate("/attendance");
-
         setError(null);
       } catch (err) {
         console.error(err);
         setError(err.message || "Something went wrong");
+        window.location.href = "https://community.samzara.in";
       } finally {
         setLoading(false);
       }
     };
 
     doLogin();
-  }, [name, login]);
+  }, [email, login,navigate]);
 
-  // ✅ UI states
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p style={{ color: "red" }}>❌ {error}</p>;
 
   return (
-    <div style={{ padding: "20px" }}>
+  <div className="p-2">
       
+      <div className="  mt-3   h-20  rounded-lg bg-gray-300 animate-pulse "> </div>
+       <div className="flex  flex-col md:flex-row mt-4 justify-between gap-4 px-4 md:px-6 text-xs sm:text-sm">
+        <div>
+      <div className="bg-gray-300 animate-pulse  rounded-2xl h-5 w-90 "></div>
+      <div className="bg-gray-300 animate-pulse mt-1 rounded-2xl h-5 w-70 "></div>
+      </div>
+      <div className="">
+        <div className="bg-gray-300 animate-pulse  rounded-2xl h-5 w-90 "></div>
+        <div className="bg-gray-300 animate-pulse mt-1 rounded-2xl h-5 w-30 "></div>
+      </div>
+      </div>
+      <div className="mt-5.5 flex  h-60  rounded-lg bg-gray-300 animate-pulse">
+      </div>
+
     </div>
   );
 }
--- frame2/src/components/Navebar.jsx
+++ frame2/src/components/Navebar.jsx
@@ -81,9 +81,51 @@ export default function Navebar() {
           {/* Right - Action Icons (Desktop) */}
           <div className=" flex  items-center gap-4.5">
             {!isAuthenticated ? (
-              <Link to="/signin">
-                
-              </Link>
+              <div className=" flex gap-4.5">
+                <div className="relative">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="23"
+                    height="23"
+                    color="#666"
+                  >
+                    <path
+                      d="M19 8h-2v3h-3v2h3v3h2v-3h3v-2h-3zM4 8a3.91 3.91 0 0 0 4 4 3.91 3.91 0 0 0 4-4 3.91 3.91 0 0 0-4-4 3.91 3.91 0 0 0-4 4zm6 0a1.91 1.91 0 0 1-2 2 1.91 1.91 0 0 1-2-2 1.91 1.91 0 0 1 2-2 1.91 1.91 0 0 1 2 2zM4 18a3 3 0 0 1 3-3h2a3 3 0 0 1 3 3v1h2v-1a5 5 0 0 0-5-5H7a5 5 0 0 0-5 5v1h2z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="relative">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="23"
+                    height="23"
+                    color="#666"
+                    fill="currentColor"
+                  >
+                    <path d="M20 2H4c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h3v3.766L13.277 18H20c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm0 14h-7.277L9 18.234V16H4V4h16v12z"></path>
+                    <circle cx="15" cy="10" r="2"></circle>
+                    <circle cx="9" cy="10" r="2"></circle>
+                  </svg>
+                </div>
+                <div className="relative">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    width="23"
+                    height="23"
+                    color="#666"
+                  >
+                    <path
+                      d="M19 13.586V10c0-3.217-2.185-5.927-5.145-6.742C13.562 2.52 12.846 2 12 2s-1.562.52-1.855 1.258C7.185 4.074 5 6.783 5 10v3.586l-1.707 1.707A.996.996 0 0 0 3 16v2a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1v-2a.996.996 0 0 0-.293-.707L19 13.586zM19 17H5v-.586l1.707-1.707A.996.996 0 0 0 7 14v-4c0-2.757 2.243-5 5-5s5 2.243 5 5v4c0 .266.105.52.293.707L19 16.414V17zm-7 5a2.98 2.98 0 0 0 2.818-2H9.182A2.98 2.98 0 0 0 12 22z"
+                      fill="currentColor"
+                    ></path>
+                  </svg>
+                </div>
+                <div className="w-[27px] h-[27px] rounded-full object-cover bg-gray-400 animate-pulse" ></div>
+              </div>
             ) : (
               <>
                 <div className="relative">
