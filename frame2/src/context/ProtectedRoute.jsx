7391f27078c061cc962003ab336f80c129c6b1e6
bkapil002 <kapilabrahm@gmail.com>
2025-09-18 14:11:53 +0545

    some changes

--- frame2/src/context/ProtectedRoute.jsx
+++ frame2/src/context/ProtectedRoute.jsx
@@ -1,43 +1,43 @@
 // src/components/ProtectedRoute.jsx
-// import { Navigate } from "react-router-dom";
-// import { useAuth } from "./AuthContext";
-
-// const ProtectedRoute = ({ children }) => {
-//   const { isAuthenticated, loading } = useAuth();
-
-//   if (loading) {
-//     return <div className="flex justify-center items-center h-screen">Loading...</div>; // 👈 prevent redirect until check is done
-//   }
-
-//   if (!isAuthenticated) {
-//     return <Navigate to="/signin" replace />;
-//   }
-
-//   return children;
-// };
-
-// export default ProtectedRoute;
-
+import { Navigate } from "react-router-dom";
 import { useAuth } from "./AuthContext";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, loading } = useAuth();
 
   if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        Loading...
-      </div>
-    ); 
+    return <div className="flex justify-center items-center h-screen">Loading...</div>; // 👈 prevent redirect until check is done
   }
 
   if (!isAuthenticated) {
-     
-    window.location.href = "https://community.samzara.in";
-    return null;
+    return <Navigate to="/signin" replace />;
   }
 
   return children;
 };
 
 export default ProtectedRoute;
+
+// import { useAuth } from "./AuthContext";
+
+// const ProtectedRoute = ({ children }) => {
+//   const { isAuthenticated, loading } = useAuth();
+
+//   if (loading) {
+//     return (
+//       <div className="flex justify-center items-center h-screen">
+//         Loading...
+//       </div>
+//     ); 
+//   }
+
+//   if (!isAuthenticated) {
+     
+//     window.location.href = "https://community.samzara.in";
+//     return null;
+//   }
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
