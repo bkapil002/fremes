a7dc6f729d7d4fb4395101f80e4b245dbe97cf1a
bkapil002 <kapilabrahm@gmail.com>
2025-09-27 18:33:49 +0545

    change

--- Backend/Emailserver/emailServer.js
+++ Backend/Emailserver/emailServer.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 class EmailServer {
   constructor() {
     this.transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, 
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
