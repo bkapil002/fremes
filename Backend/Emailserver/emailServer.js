41c38e8de0449c6c4a47d8ccd7d6e816d19f956c
bkapil002 <kapilabrahm@gmail.com>
2025-09-29 13:30:30 +0545

    email-update

--- Backend/Emailserver/emailServer.js
+++ Backend/Emailserver/emailServer.js
@@ -5,7 +5,6 @@ class EmailServer {
     this.transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
-      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
