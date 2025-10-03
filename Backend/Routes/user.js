a7d8eaaeac7e8fd21ea382cf3805e34aa83b8ee6
bkapil002 <kapilabrahm@gmail.com>
2025-10-03 16:54:59 +0545

    backend-token

--- Backend/Routes/user.js
+++ Backend/Routes/user.js
@@ -102,8 +102,9 @@ router.get('/auth/:decodedEmail', async(req,res)=>{
         res.cookie("token", token, {
        httpOnly: true,
         secure: process.env.NODE_ENV === "production", 
-        sameSite: "strict", 
-        maxAge: 5 * 60 * 1000, 
+        sameSite: "None", 
+        secure: true, 
+         maxAge: 7 * 24 * 60 * 60 * 1000, 
        })
        .json({
         token,
